ee472dfe5ec045b657dee5676eba600036cb6aa9
Admin <Admin@Lenovo-PC>
2018-03-04 21:56:51 +0530

    Added few more derived columns

--- SparkSqlDemo/src/main/scala/example/GeoIsTransformation.scala
+++ SparkSqlDemo/src/main/scala/example/GeoIsTransformation.scala
@@ -74,6 +74,19 @@ object GeoIsTransformation extends App {
        when( cvNoPersUnit=== "8" , "8+ Persons").
        otherwise("Unknown")
        
+         
+ 
+   val deriveCountrySize = ( urbanCountryCode : Column) => when( urbanCountryCode=== "1" , "1 Metro Pop 1M+").
+       when( urbanCountryCode=== "2" , "2 Metro Pop 250K - 1M").
+       when( urbanCountryCode=== "3" , "3 Metro Pop 250K").
+       when( urbanCountryCode=== "4" , "4 Urban 20K Metro adjacent").
+       when( urbanCountryCode=== "5" , "5 Urban 20K not adjacent").
+       when( urbanCountryCode=== "6" , "6 Urban 2500 - 19999 adjacent").
+       when( urbanCountryCode=== "7" , "7 Urban 2500 - 19999 not adjacent").
+       when( urbanCountryCode=== "8" , "8 Rural < 2500 urban adjacent").
+       when( urbanCountryCode=== "9" , "9 Rural < 2500 urban not adjacent").
+       otherwise("Unknown")    
+       
    
    val getHHPrimePersonExpression = "cv_p2_ptyp='P' and cv_p2_gender='F'"
    
@@ -96,6 +109,18 @@ object GeoIsTransformation extends App {
        .otherwise("Unknown")
      }
      
+     val determinePrimaryPersonGenderWithGivenAge = ( gender : String , p2Gender : Column, p2Age:Column, p1Gender : Column, p1Age:Column,low : Any , high : Any) => {
+       when(determineAge(p2Age,p1Age).between(low, high).and(when(expr(getHHPrimePersonExpression)===true,p2Gender).otherwise(p1Gender).===(gender))===true,"Yes").
+       otherwise("No")
+       
+     }
+ 
+     val determinePrimaryPersonGenderWithGivenMaxAge = ( gender : String , p2Gender : Column, p2Age:Column, p1Gender : Column, p1Age:Column,low : Any ) => {
+       when(determineAge(p2Age,p1Age).>(low).and(when(expr(getHHPrimePersonExpression)===true,p2Gender).otherwise(p1Gender).===(gender))===true,"Yes").
+       otherwise("No")
+       
+     }
+ 
      val applyChildPresTypes = ( anyColumn : Column,cvChildPres0_18 :Column) => anyColumn.and(cvChildPres0_18.isin("00","0U","5N","5U")) 
      
      val applyChildAgeChecks = ( col : Column ) => col.isin("1Y","5Y")
@@ -192,7 +217,27 @@ object GeoIsTransformation extends App {
        and(destDf("cv_no_child_unit").cast(IntegerType).>=(1)),"Yes").otherwise("No"))
        
    .withColumn("hh_age_of_old_child", determineAgeOfOldChild())
-      
+   //.withColumn("hh_presence_of_gender",)
+   //.withColumn("hh_millennial_male",)
+   //.withColumn("hh_millennial_male",)
+   //.withColumn("hh_millennial_male",)
+   //.withColumn("hh_millennial_male",)
+   //.withColumn("hh_millennial_male",)
+   .withColumn("hh_millennial_male", determinePrimaryPersonGenderWithGivenAge("M" , destDf("cv_p2_gender"), destDf("cv_p2_com_age"), destDf("cv_p1_gender"), destDf("cv_p1_com_age"),19,39))   
+   .withColumn("hh_millennial_female", determinePrimaryPersonGenderWithGivenAge("F" , destDf("cv_p2_gender"), destDf("cv_p2_com_age"), destDf("cv_p1_gender"), destDf("cv_p1_com_age"),19,39))   
+   .withColumn("hh_gen_x_male", determinePrimaryPersonGenderWithGivenAge("M" , destDf("cv_p2_gender"), destDf("cv_p2_com_age"), destDf("cv_p1_gender"), destDf("cv_p1_com_age"),40,50))   
+   .withColumn("hh_gen_x_female", determinePrimaryPersonGenderWithGivenAge("F" , destDf("cv_p2_gender"), destDf("cv_p2_com_age"), destDf("cv_p1_gender"), destDf("cv_p1_com_age"),40,50))   
+   .withColumn("hh_young_boomer_male", determinePrimaryPersonGenderWithGivenAge("M" , destDf("cv_p2_gender"), destDf("cv_p2_com_age"), destDf("cv_p1_gender"), destDf("cv_p1_com_age"),51,60))   
+   .withColumn("hh_young_boomer_female", determinePrimaryPersonGenderWithGivenAge("F" , destDf("cv_p2_gender"), destDf("cv_p2_com_age"), destDf("cv_p1_gender"), destDf("cv_p1_com_age"),51,60))   
+   .withColumn("hh_post_boomer_male", determinePrimaryPersonGenderWithGivenMaxAge("M" , destDf("cv_p2_gender"), destDf("cv_p2_com_age"), destDf("cv_p1_gender"), destDf("cv_p1_com_age"),61))   
+   .withColumn("hh_post_boomer_female", determinePrimaryPersonGenderWithGivenMaxAge("F" , destDf("cv_p2_gender"), destDf("cv_p2_com_age"), destDf("cv_p1_gender"), destDf("cv_p1_com_age"),61))   
+
+   .withColumn("hh_county_size", deriveCountrySize(destDf("cv_cen_rur_urban_county_szcd")))   
+   .withColumn("hh_urban_suburban_rural", when(destDf("cv_cbsa_type")==="A","Urban").
+       when(destDf("cv_cbsa_type")==="B","Suburban").
+       when(destDf("cv_cbsa_type")==="C","Rural").otherwise("Unknown"))
+   .withColumn("HH_County", concat(destDf("cv_county_cd"),lit("-"),destDf("cv_county_name")))
+   
    destDf.
    write.mode("overwrite").saveAsTable("exp_geo_is_cg")
  }
